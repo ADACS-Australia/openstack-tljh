c7e6467af6435bfde73250fe5e8385f2adf20081
David Liptai <dliptai@swin.edu.au>
2022-08-10 10:17:44 +1000

    create build script to run packer AND change the murano name

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -eu
+
+IMAGE_NAME="ADACS The Littlest JupyterHub (Ubuntu 20.04 LTS Focal) [dev]"
+MURANO_NAME="ADACS The Littlest JupyterHub (Ubuntu 20.04 LTS Focal)" # Don't change this
+
+packer build -var "image_name=${IMAGE_NAME}" packer.json
+
 openstack image set --property \
-  murano_image_info='{"title": "ADACS The Littlest JupyterHub (Ubuntu 20.04 LTS Focal)", "type": "linux.ubuntu"}' \
-  "ADACS The Littlest JupyterHub (Ubuntu 20.04 LTS Focal)"
+  murano_image_info="{'title': '${MURANO_NAME}', 'type': 'linux.ubuntu'}" \
+  "${IMAGE_NAME}"

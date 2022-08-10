26f316e8fce0268c099f2a4b83bf744202f807fa
David Liptai <dliptai@swin.edu.au>
2022-08-10 10:56:06 +1000

    echo murano name change

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ MURANO_NAME="ADACS The Littlest JupyterHub (Ubuntu 20.04 LTS Focal)" # Don't cha
 
 packer build -var "image_name=${IMAGE_NAME}" build.pkr.hcl
 
+echo "--------- image: ${IMAGE_NAME} -----------"
+echo "    Setting Murano title to: ${MURANO_NAME} "
 openstack image set --property \
   murano_image_info="{'title': '${MURANO_NAME}', 'type': 'linux.ubuntu'}" \
   "${IMAGE_NAME}"

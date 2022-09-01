b52e41cd9098e86661379a9626faa9875ffdb9c9
David Liptai <dliptai@swin.edu.au>
2022-09-01 17:55:52 +1000

    ignore project quotas

--- roles/quota/files/quota-msg.sh
+++ roles/quota/files/quota-msg.sh
@@ -1,8 +1,9 @@
-if groups | grep -q jupyterhub-admins && quotaon -p -a | grep -q "is off"; then
+if groups | grep -q jupyterhub-admins && quotaon -p -a | grep -v "project quota" | grep -q "is off"; then
+
 cat << EOF
 Warning:
 
-$(quotaon -p -a | grep "is off")
+$(quotaon -p -a | grep -v "project quota" | grep "is off")
 
 You may need to reboot the system to enable the quota kernel modules.
 ("sudo reboot")

6cfb7e97e56c99f1e5b54a329cb0616fa96285b3
David Liptai <dliptai@swin.edu.au>
2022-09-01 15:36:05 +1000

    neater print

--- roles/quota/files/start-quota.sh
+++ roles/quota/files/start-quota.sh
@@ -7,13 +7,15 @@ fi
 errcode=0
 
 for DEV in $(ls /dev/vd[a-z]1); do
+  echo
   echo "---> Enabling quotas on: $DEV"
   mount -o remount "$DEV"   || {((++errcode)) && continue;}
   quotaoff "$DEV"           || {((++errcode)) && continue;}
   quotacheck -cvugm "$DEV"  || {((++errcode)) && continue;}
   quotaon -vug "$DEV"       || {((++errcode)) && continue;}
-  echo
 done
+
+echo
 
 if [ "$errcode" != 0 ]; then
   echo 'ERROR: You may need to reboot the system to load the quota kernel modules ("sudo reboot")'

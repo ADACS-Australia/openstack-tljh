f304935820b45062e8fd32bc95197d75d680fb91
David Liptai <dliptai@swin.edu.au>
2023-03-01 13:42:56 +1100

    cleaner warning message

--- roles/quota/files/quota-msg.sh
+++ roles/quota/files/quota-msg.sh
@@ -1,12 +1,13 @@
 if groups | grep -q jupyterhub-admins && quotaon -p -a | grep -v "project quota" | grep -q "is off"; then
 
 cat << EOF
-Warning:
-
+--------------------------------------------------------------------------------
+WARNING!
 $(quotaon -p -a | grep -v "project quota" | grep "is off")
 
-You may need to reboot the system to enable the quota kernel modules.
-("sudo reboot")
+  You may need to reboot the system to enable the quota kernel modules.
+  ("sudo reboot")
+--------------------------------------------------------------------------------
 
 EOF
 fi
